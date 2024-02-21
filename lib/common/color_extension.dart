9591a9c85a9efd366d652ce05ced9aed812507b1
artichaudhary <chaudhary.aarti1998@gmail.com>
2024-02-21 14:15:42 +0530

    Initial commit

---
+++ lib/common/color_extension.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class TColor{
+  static Color get primary => const Color(0xFFFC6011);
+  static Color get primaryText => const Color(0xFF4A4B4D);
+  static Color get secondaryText => const Color(0xFF7C7D7E);
+  static Color get textField => const Color(0xFFF2F2F2);
+  static Color get placeholder => const Color(0xFFB6B7B7);
+
+
+}
